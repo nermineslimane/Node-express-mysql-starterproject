1b11ee7f5fc6d2c1aee85af676e33c7ac8b3b333
nermineslimane <slimane.nermine@esprit.tn>
2022-01-14 17:29:32 +0100

    server PORT & setup
    If the status of the application is set to production the port to use will be defined in the .env file or else if the status is set to development there's a different port for that as well

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+//const config = require('./app/config/auth.config');
+
+const app = express();
+app.use(express.json());
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+const httpServer = require('http').createServer(app);
+let PORT;
+process.env.STATUS === 'production'
+  ? (PORT = process.env.PROD_PORT)
+  : (PORT = process.env.DEV_PORT);
+console.log(PORT);
+httpServer.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
